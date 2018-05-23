05e1bf337ee721a8e302307935060d0e44b5a388
SukanyaBo <Sukanya.Cs56@gmail.com>
2018-05-23 14:14:37 +0700

    test

--- fbConfig.php
+++ fbConfig.php
@@ -9,7 +9,7 @@ require_once 'inc/facebook.php';
  */
 $appId = '229494454301142'; 								//Facebook App ID(create this from 																				developer.facebook.com)
 $appSecret = 'c6a580babc9f308716c4f4f21f37b428'; 			// Facebook App Secret
-$redirectURL = 'http://localhost:8888/beauty24/login/'; // Callback URL i.e. page where you want to redirect after 														login, this should be same as oauth login in dashboard
+$redirectURL = 'https://tes-tlogin-facebook.herokuapp.com/'; // Callback URL i.e. page where you want to redirect after 														login, this should be same as oauth login in dashboard
 //$redirectURL = 'http://cp.chasingpapers.com/'; // Callback URL
 //$redirectURL = 'http://cp4.chasingpapers.com/';
 $fbPermissions = 'email';  //Required facebook permissions
